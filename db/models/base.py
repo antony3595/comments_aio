99c3c53e2f81084d56f1b25dbb764a181fc6140b
Anton Yatchenko <antony3595@gmail.com>
2024-09-05 21:53:04 +0600

    (MAS-5) + remove useless kwargs at sqlalchemy BaseModel

--- db/models/base.py
+++ db/models/base.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 class BaseModel(Base):
     __abstract__ = True
 
-    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
 
     def __repr__(self):
