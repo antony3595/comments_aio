1ab4f96db03918b3c40fa18fb2e02e68a36a472d
Anton Yatchenko <antony3595@gmail.com>
2024-09-16 22:57:16 +0600

    (MAS-5) + add NewsCategory and UserCategorySubscription models

--- db/connections/postgres.py
+++ db/connections/postgres.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from config import settings
 
-engine = create_async_engine(settings.DB_CONNECTION_STRING.get_secret_value(), echo=settings.DEBUG)
+engine = create_async_engine(settings.DB_CONNECTION_STRING.get_secret_value(), echo=settings.DEBUG, pool_size=40, max_overflow=20)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
--- db/models/news.py
+++ db/models/news.py
@@ -1,8 +1,43 @@
-from sqlalchemy import Column, VARCHAR
+import enum
+
+from sqlalchemy import Column, VARCHAR, ForeignKey, UniqueConstraint, Enum
+from sqlalchemy.orm import relationship
 
 from db.models.base import BaseModel
+
+
+class NewsTypeEnum(enum.Enum):
+    WORLD_EVENTS = "Мировые события"
+    POLITICS = "Политика"
+    ECONOMY = "Экономика"
+    SCIENCE_AND_TECHNOLOGY = "Наука и технологии"
+    HEALTHCARE = "Здравоохранение"
+    ENVIRONMENT = "Экология"
+    CULTURE_AND_ARTS = "Культура и искусство"
+    SPORTS = "Спорт"
+    SOCIETY = "Общество"
+    ENTERTAINMENT = "Развлечения"
 
 
 class News(BaseModel):
     __tablename__ = 'news'
     title = Column(VARCHAR(255), nullable=False)
+    categories = relationship("NewsCategory", back_populates="news")
+
+
+class NewsCategory(BaseModel):
+    __tablename__ = 'news_categories'
+    category = Column(Enum(NewsTypeEnum), nullable=False)
+    news_id = Column(ForeignKey("news.id"), nullable=False)
+    news = relationship("News", back_populates="categories")
+
+    __table_args__ = (UniqueConstraint(category, news_id),)
+
+
+class UserCategorySubscription(BaseModel):
+    __tablename__ = 'users_categories_subscriptions'
+    category = Column(Enum(NewsTypeEnum), nullable=False)
+    user_id = Column(ForeignKey("auth_users.id", ondelete="CASCADE"), nullable=False)
+    user = relationship("AuthUser", backref="categories")
+
+    __table_args__ = (UniqueConstraint(category, user_id),)
